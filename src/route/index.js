d3ef15033cf59ed0b66dbbffb408a4e0c4d68896
OlhaSuhoterina <suhoterina.olya@gmail.com>
2023-05-19 10:05:03 +0300

    slack

--- src/route/index.js
+++ src/route/index.js
@@ -548,4 +548,16 @@ router.get('/template-3', function (req, res) {
 
 // ================================================================
 
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/slack', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('slack', {
+    layout: null,
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
 module.exports = router
